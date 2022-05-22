9217df189df10a5c961567906b50b3932dd97329
Kevin George <69797975+kevinsunil@users.noreply.github.com>
2022-05-22 10:54:32 +0530

    Create treasureisland.py

---
+++ day3/treasureisland.py
@@ -0,0 +1,52 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+Direction = input("You are being chased by bandits, you need to loose them, You reach a crossroad where you  can either take left or right. Which one would you choose ")
+if Direction == 'right':
+  print("You got caught, game over")
+else:
+  travel = input("You were successful in getting rid of the bandits, now you have reached a waterbody. Would you lite to swim or wait by the water for boat ")
+if travel == 'swim':
+  print("The lake had pirhanas which ended up eating you, game over")
+else:
+  door = input("You arrive at the island unharmed. There is a cave with three entrances each having a carving on top. One with lion, one with eagle and one with snake. Which colour do you choose? ")
+if door == 'lion':
+  print("you dropped to a pool of lava, game over")
+elif door == 'eagle':
+  print("you walk for about five minutes before something hard hits against your leg. you look down and find a chest.... you open it and find........")
+  print('''888                                                          
+888                                                          
+888                                                          
+888888888d888 .d88b.  8888b. .d8888b 888  888888d888 .d88b.  
+888   888P"  d8P  Y8b    "88b88K     888  888888P"  d8P  Y8b 
+888   888    88888888.d888888"Y8888b.888  888888    88888888 
+Y88b. 888    Y8b.    888  888     X88Y88b 888888    Y8b.     
+ "Y888888     "Y8888 "Y888888 88888P' "Y88888888     "Y8888 
+''')
+elif door == 'snake':
+  print("you walk for about five minutes before something hard hits against your leg. you look down and find a chest.... you open it and find........ snakes inside... ooops ... game over...")

bd31a3fe551d125a879add2ca064905a3e80a38b
Kevin George <69797975+kevinsunil@users.noreply.github.com>
2022-05-29 18:03:05 +0530

    Update hangman.py

--- day7/hangman.py
+++ day7/hangman.py
@@ -1,3 +1,4 @@
+#Step 1 
 import random
 from hangman_words import word_list
 from hangman_art import logo
@@ -7,9 +8,9 @@ word = random.choice(word_list)
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 from hangman_art import stages
 check=[]
-for i in range(0,len(word)):
-    check.append("_")
-print(check)
+for _ in range(len(word)):
+    check += "_"
+print(f"{' '.join(check)}")
 gameover = 0
 lives =6 
 while(gameover == 0):
@@ -22,23 +23,23 @@ while(gameover == 0):
       if word[char] == character:
         check[char]= character
         check3 += 1
-      else:
-        if check[char] == '_':
-          check[char]= "-"
-        else:
-          check[char] = check[char]
+      # else:
+      #   if check[char] == '_':
+      #     check[char]= "-"
+      #   else:
+      #     check[char] = check[char]
   if check3 == 0:
     lives -=1
     if lives == 0:
       print(stages[lives])
-      print("you lost.. Game Over")
+      print(f"you lost.... the word was {word}.. Game Over")
       gameover =1
     else:
       print("oops you lost a life")
       print(stages[lives])
-      print(check)
+      print(f"{' '.join(check)}")
   else:
-    print(check)
+    print(f"{' '.join(check)}")
     check2 =0
     for char in range(0,len(word)):
       if word[char] != check[char]:
@@ -46,3 +47,4 @@ while(gameover == 0):
     if check2 == 0:
       print("Yay you guessed the word")
       gameover += 1
+#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.

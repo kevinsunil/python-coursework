6a7d4ae72f0c8fce5a41a4d7bb23c3e34d1b0898
Kevin George <69797975+kevinsunil@users.noreply.github.com>
2022-10-28 12:51:59 +0530

    highscore_feature added

--- day20/scoreboard.py
+++ day20/scoreboard.py
@@ -8,6 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = -1
+        with open('highscore.txt') as file:
+            self.highscore = int(file.read())
         self.penup()
         self.hideturtle()
         self.goto(0, 270)
@@ -17,8 +19,18 @@ class Scoreboard(Turtle):
     def update_score(self):
         self.clear()
         self.score += 1
-        self.write(f"SCORE:{self.score} ", False, align=ALIGNMENT, font=FONT)
+        self.write(f"SCORE:{self.score} HIGHSCORE:{self.highscore}", False, align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER ", False, align=ALIGNMENT, font=FONT)
+    def reset_score(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+        with open('highscore.txt', mode='w') as file:
+            file.write(str(self.highscore))
+        self.score = -1
+        self.update_score()
+
+
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER ", False, align=ALIGNMENT, font=FONT)

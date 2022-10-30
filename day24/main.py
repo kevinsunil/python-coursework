1f3f6c3b2a6819ed56600ced01868018565b2471
Kevin George <69797975+kevinsunil@users.noreply.github.com>
2022-10-30 22:28:24 +0530

    Update main.py

--- day24/main.py
+++ day24/main.py
@@ -6,3 +6,13 @@
 # Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
 # Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
 # Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+text = "[name]"
+with open("./Input/Names/invited_names.txt") as guest:
+    guestlist = guest.readlines()
+with open("./Input/Letters/starting_letter.txt") as letter:
+    sample_letter = letter.read()
+    for name in guestlist:
+        name = name.strip()
+        final_letter = sample_letter.replace(text, name)
+        with open(f"./Output/ReadyToSend/{name}.txt","w") as email:
+            email.write(final_letter)
